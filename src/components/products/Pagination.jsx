76f40fe89879c00799209bae6374caaac67ee096
Abdelrahman Ragab <abdelrahmanragab787@gmail.com>
2024-09-26 15:34:51 +0000

    fix style

--- src/components/products/Pagination.jsx
+++ src/components/products/Pagination.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChevronRight, ChevronLeft } from "lucide-react";
+
 const Pagination = ({
   totalPages,
   currentPage,
@@ -17,27 +18,30 @@ const Pagination = ({
               currentPage === 1 && "cursor-not-allowed"
             }`}
           >
-            <ChevronLeft/>
+            <ChevronLeft />
           </button>
         </li>
         {Array.from({ length: totalPages }, (_, index) => index + 1).map(
           (page) => (
-            <button
-              key={page}
-              onClick={() => handlePageChange(page)}
-              className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
-                currentPage === page ? "text-blue-500" : "bg-gray-200"
-              }`}
-            >
-              {page}
-            </button>
+            <li key={page}>
+              <button
+                onClick={() => handlePageChange(page)}
+                className={`flex items-center justify-center px-3 h-8 leading-tight ${
+                  currentPage === page
+                    ? "text-blue-500 font-bold bg-gray-200"
+                    : "text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+                }`}
+              >
+                {page}
+              </button>
+            </li>
           )
         )}
         <li>
           <button
             disabled={currentPage === totalPages}
             onClick={() => setCurrentPage(currentPage + 1)}
-            className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+            className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 ${
               currentPage === totalPages && "cursor-not-allowed"
             }`}
           >
